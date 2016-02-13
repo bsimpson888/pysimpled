0094bc6ed1083c01619d24b5b8d0d4c4e28e7884
Marco Bartel <bsimpson888@gmail.com>
2016-02-13 07:36:18 +0100

    added SimpleLog and SimplePid

--- sources/pysimpled/SimpleDaemon.py
+++ sources/pysimpled/SimpleDaemon.py
@@ -7,6 +7,7 @@ import os
 import sys
 import platform
 
+
 import time
 
 __author__ = 'Marco Bartel'
@@ -17,34 +18,86 @@ class SimpleDaemonException(Exception):
         super(SimpleDaemonException, self).__init__(*args, **kwargs)
 
 
-class SimpleDaemonLogger(object):
-    class SimpleDaemonInnerLogger(object):
-        def __init__(self, logger, level):
+class SimpleLogger(object):
+    class SimpleInnerLogger(object):
+        def __init__(self, logger, level, both=False, oldstream=None):
             self.logger = logger
             self.level = level
+            self.both = both
+            self.oldstream = oldstream
 
         def write(self, message):
             # Only log if there is a message (not just a new line)
+            if self.both:
+                    self.oldstream.write(message)
+
             if message.rstrip() != "":
                 self.logger.log(self.level, message.rstrip())
 
-    def __init__(self, logPath):
+
+    def __init__(self, logPath, both=False):
+
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+        self.both = both
 
         self.logPath = logPath
+
         if not os.path.exists(os.path.dirname(self.logPath)):
             os.makedirs(os.path.dirname(self.logPath))
 
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
         handler = logging.handlers.TimedRotatingFileHandler(self.logPath, when="midnight", backupCount=3)
         formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
         handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        self.logger.addHandler(handler)
 
-        # Replace stdout with logging to file at INFO level
-        sys.stdout = self.SimpleDaemonInnerLogger(logger, logging.INFO)
-        # Replace stderr with logging to file at ERROR level
-        sys.stderr = self.SimpleDaemonInnerLogger(logger, logging.ERROR)
+    def __enter__(self):
+        sys.stderr = self.SimpleInnerLogger(self.logger, logging.ERROR, both=self.both, oldstream=sys.stderr)
+        sys.stdout = self.SimpleInnerLogger(self.logger, logging.INFO, both=self.both, oldstream=sys.stdout)
+
+    def __exit__(self, type, value, traceback):
+        # time.sleep(1)
+        if type is None:
+            sys.stdout = self.old_stdout
+            sys.stderr = self.old_stderr
+
+class SimplePid(object):
+    def __init__(self, pidPath):
+
+        self._pid = os.getpid()
+
+        self.pidPath = pidPath
+        if not os.path.exists(os.path.dirname(self.pidPath)):
+            os.makedirs(os.path.dirname(self.pidPath))
+
+    def __enter__(self):
+        self.checkPidFile()  # exit if exists
+        self.createPidFile()  # create PID file
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.removePidFile()
+
+    def pid(self):
+        return self._pid
+
+    def createPidFile(self):
+        if not os.path.exists(os.path.dirname(self.pidPath)):
+            os.makedirs(os.path.dirname(self.pidPath))
+        fd = open(self.pidPath, "w")
+        fd.write(unicode(self.pid()))
+        fd.close()
+
+    def checkPidFile(self):
+        if os.path.isfile(self.pidPath):
+            print "PID File exists already. Exiting..."
+            sys.exit()
+
+    def removePidFile(self):
+        if os.path.isfile(self.pidPath):
+            os.remove(self.pidPath)
 
 
 class SimpleDaemon(object):
@@ -69,7 +122,7 @@ class SimpleDaemon(object):
             print "logPath:", self.logPath
 
         if not self.debug:
-            self.logger = SimpleDaemonLogger(self.logPath)
+            self.logger = SimpleLogger(self.logPath)
 
     def pid(self):
         return self._pid
@@ -81,7 +134,6 @@ class SimpleDaemon(object):
 
     def __exit__(self, type, value, traceback):
         self.removePidFile()
-
 
     def createPidFile(self):
         if not os.path.exists(os.path.dirname(self.pidPath)):
@@ -101,6 +153,8 @@ class SimpleDaemon(object):
 
 
 if __name__ == '__main__':
-    SimpleDaemon.debug = True
-    with SimpleDaemon("test") as testDaemon:
-        print testDaemon.pid()
+    with SimpleLogger('c:\\temp\\SimpleDaemon\\test.log', both=True):
+        with SimplePid('c:\\temp\\SimpleDaemon\\test.lock'):
+            time.sleep(10)
+            print "hello"
+            error()
