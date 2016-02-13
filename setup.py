489902dfe113f8ededb650def380905f0ee88b6e
Marco Bartel <bsimpson888@gmail.com>
2016-02-13 11:05:54 +0100

    added upload_pypi script to remember the commands

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pysimpled',
-    version='1.0.0',
+    version='1.0.1',
     package_dir={'pysimpled': "sources/pysimpled"},
     packages=['pysimpled'],
     url='https://github.com/bsimpson888/pysimpled',
